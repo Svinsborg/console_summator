b7c1171297678bbdeed1e5cfd5c59a3d0eb3388c
Andy <viktorov.a.m@mail.ru>
2022-12-11 15:44:39 +0300

    Ready

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,60 +7,91 @@ public class Main {
         Scanner scn = new Scanner(System.in);
         System.out.println("Input:");
         String input = scn.nextLine();
-        String inputEdit = input.replaceAll("\\s+","");
+        String inputEdit = (input.replaceAll("\\s+", "")).toUpperCase();
 
-        int a,b;
+        int a = 0, b = 0;
 
         String[] operators = {"+", "-", "/", "*"};
         String[] separator = {"\\+", "-", "/", "\\*"};
-        int operatorsIndex =  -1 ;
+        int operatorsIndex = -1;
         for (int i = 0; i < operators.length; i++) {
-            if(inputEdit.contains(operators[i])){
+            if (inputEdit.contains(operators[i])) {
                 operatorsIndex = i;
                 break;
             }
         }
-        if(operatorsIndex == -1){
-            System.out.println("Некорректное выражение");
+
+        if (operatorsIndex == -1) {
+            ErrOutput("строка не является математической операцией");
             return;
         }
 
         String[] data = inputEdit.split(separator[operatorsIndex]);
+        if(data.length > 2){
+            ErrOutput("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            System.exit(0);
+        }
 
         MyConverter converter = new MyConverter();
 
-        if(converter.checkRoman(data[0]) == MyConverter.checkRoman(data[1])){
+        boolean romanKey = false;
 
-            boolean romanKey =  converter.checkRoman(data[0]);
-            if(romanKey = true){
+        if (MyConverter.checkRoman(data[0]) == MyConverter.checkRoman(data[1])) {
+
+            romanKey = MyConverter.checkRoman(data[0]);
+
+            if (romanKey) {
                 a = converter.toInt(data[0]);
                 b = converter.toInt(data[1]);
-                System.out.println("Output: \n" + a + " " + b);
+                if(!RangeCheck(a,b)) System.exit(0);
+                System.out.println("Conxert to arabian: \n" + a + " " + b);
+            } else {
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+                if(!RangeCheck(a,b)) System.exit(0);
             }
+
+            int result;
+            switch (operators[operatorsIndex]) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    result = a / b;
+                    break;
+            }
+
+            System.out.println("Output: ");
+
+            if (romanKey) {
+                if (result < 1)  ErrOutput("в римской системе нет отрицательных чисел");
+                System.out.println(converter.ToRoman(result));
+            } else {
+                System.out.println(result);
+            }
+        } else {
+            ErrOutput("используются одновременно разные системы счисления");
         }
+    }
+    public static void ErrOutput(String message) {
+        System.out.println("Err: " + message);
+    }
 
-
-
-
-/*        a = Integer.parseInt(data[0]);
-        b = Integer.parseInt(data[1]);
-
-        int result;
-        switch (operators[operatorsIndex]){
-            case "+":
-                result = a+b;
-                break;
-            case "-":
-                result = a-b;
-                break;
-            case "*":
-                result = a*b;
-                break;
-            default:
-                result = a/b;
-                break;
+    public static boolean RangeCheck(int a, int b) {
+        if (0 > a || a > 10){
+            ErrOutput("Числа должны быть в диапазоне от 0..10 или I..X");
+            return false;
         }
-
-        System.out.println("Output: \n" + result);*/
+        else if (0 > b || b > 10){
+            ErrOutput("Числа должны быть в диапазоне от 0..10 или I..X");
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/org/example/MyConverter.java
+++ src/main/java/org/example/MyConverter.java
@@ -1,12 +1,6 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MyConverter {
-    static Map<Character, Integer> romanMap = new HashMap<>();
-
-    Map<Integer, String> arabianMap = new HashMap<>();
 
     public static int convertArabian(char roman){
         if('I' == roman) return 1;
@@ -17,6 +11,23 @@ public class MyConverter {
         else if('D' == roman) return 500;
         else if('M' == roman) return 1000;
         return 0;
+    }
+
+    public static String convertRoman(int arabian){
+        if (1000 <= arabian) return "M";
+        else if (900 <= arabian) return "CM";
+        else if (500 <= arabian) return "D";
+        else if (400 <= arabian) return "CD";
+        else if (100 <= arabian) return "C";
+        else if (90 <= arabian) return "XC";
+        else if (50 <= arabian) return "L";
+        else if (40 <= arabian) return "XL";
+        else if (10 <= arabian) return "X";
+        else if (9 <= arabian) return "IX";
+        else if (5 <= arabian) return "V";
+        else if (4 <= arabian) return "IV";
+        else if (1 <= arabian) return "I";
+        return "Err";
     }
 
     public static boolean checkRoman(String param){
@@ -45,4 +56,15 @@ public class MyConverter {
         return result;
     }
 
+    public String ToRoman(int number) {
+        String roman;
+        String arabianNum;
+        String result = "";
+        do {
+            roman = convertRoman(number);
+            number =  number - toInt(roman);
+            result = result + roman;
+        } while (number != 0);
+        return result;
+    }
 }
